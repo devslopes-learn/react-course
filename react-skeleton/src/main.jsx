4a34acb64753c7ff5cea3cd9c3fb71e1d6d74317
spentak <mprice@verisagellc.com>
2015-12-11 22:28:03 -0700

    removed projects that have repos

--- react-skeleton/src/main.jsx
+++
@@ -1,5 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var List = require('./components/List.jsx');
-
-ReactDOM.render(<List />, document.getElementById('ingredients'));
